7bb9d6a18b1245439cdf059c1fc36158524f8123
Tech-Mouse <174319994+Tech-Mouse@users.noreply.github.com>
2025-08-25 23:22:43 +0200

    Add git deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'Memoire-des-Ancetres',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
